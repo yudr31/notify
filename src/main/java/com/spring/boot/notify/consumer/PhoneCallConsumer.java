7a0be604f4be7d26d43058d48abfbe1af151dd13
yuderen <yuderen@wesmartclothing.com>
2019-11-19 14:32:51 +0800

    添加日志信息

--- src/main/java/com/spring/boot/notify/consumer/PhoneCallConsumer.java
+++ src/main/java/com/spring/boot/notify/consumer/PhoneCallConsumer.java
@@ -3,6 +3,7 @@ package com.spring.boot.notify.consumer;
 import com.spring.boot.common.constant.MqConstant;
 import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.type.PhoneNumber;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,14 @@ import java.net.URI;
  * @author yuderen
  * @version 2019/8/15 15:42
  */
+@Slf4j
 @Component
 @RabbitListener(queues = MqConstant.PHONE_CALL_NOTIFY_KEY)
 public class PhoneCallConsumer extends PhoneNotifyProcesser{
 
     @RabbitHandler
     public void process(String msg){
-        System.out.println("电话通知：" + msg);
+        log.info("电话通知：{}", msg);
         super.process(msg);
     }
 
@@ -28,7 +30,7 @@ public class PhoneCallConsumer extends PhoneNotifyProcesser{
         try {
             URI uri = URI.create("http://demo.twilio.com/docs/voice.xml");
             Call call = Call.creator(to, from, uri).create();
-            System.out.println(call.getSid());
+            log.info("电话通知成功：{}", call.getSid());
         } catch (Exception e){
             e.printStackTrace();
         }
--- src/main/java/com/spring/boot/notify/consumer/PhoneMessageConsumer.java
+++ src/main/java/com/spring/boot/notify/consumer/PhoneMessageConsumer.java
@@ -3,6 +3,7 @@ package com.spring.boot.notify.consumer;
 import com.spring.boot.common.constant.MqConstant;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -11,19 +12,20 @@ import org.springframework.stereotype.Component;
  * @author yuderen
  * @version 2019/8/15 15:42
  */
+@Slf4j
 @Component
 @RabbitListener(queues = MqConstant.PHONE_MESSAGE_NOTIFY_KEY)
 public class PhoneMessageConsumer extends PhoneNotifyProcesser{
 
     @RabbitHandler
     public void process(String msg){
-        System.out.println("短信通知：" + msg);
+        log.info("短信通知：{}", msg);
         super.process(msg);
     }
 
     public void doNotify(PhoneNumber from, PhoneNumber to, String msg){
         Message message = Message.creator(to, from, msg).create();
-        System.out.println(message.getSid());
+        log.info("短信通知：{}",message.getSid());
     }
 
 }
